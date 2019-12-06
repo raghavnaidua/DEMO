437232b008f3d7e989dfed2f282d1180cdb40a54
raghavnaidua <58593543+raghavnaidua@users.noreply.github.com>
2019-12-06 17:45:44 +0530

    Update add.c

--- add.c#include <stdio.h> int main() {         int number1, number2, sum;          printf("Enter two integers: ");     scanf("%d %d", &number1, &number2);/add.c
+++ add.c#include <stdio.h> int main() {         int number1, number2, sum;          printf("Enter two integers: ");     scanf("%d %d", &number1, &number2);/add.c
@@ -6,7 +6,7 @@ int main() {
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
     // calculating sum
-    sum = number1 + number2;      
+    sum = number3 + number2;      
     
     printf("%d + %d = %d", number1, number2, sum);
     return 0;
